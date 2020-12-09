d8ac24799e296d0a2cb22b35f5a03e080b9516bf
kengne ruphine <ruphinekengne@gmail.com>
2020-12-09 14:21:52 +0100

    using templates to display complex html and passing variables to templates as context

--- django_auth/auth_app/blog/views.py
+++ django_auth/auth_app/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author':'Grace',
+        'title':'Django Python',
+        'content':'First post',
+        'date_posted':'December 09, 2020'
+    },
+    {
+        'author':'Jane Doe',
+        'title':'Django Python 2',
+        'content':'Second post',
+        'date_posted':'December 10, 2020'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog home</h1>')
+    context = {
+        'posts':posts   #create a dictionary and set the value of the key to the posts array above
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About </h1>')
+    return render(request, 'blog/about.html', {'title':'About'})
